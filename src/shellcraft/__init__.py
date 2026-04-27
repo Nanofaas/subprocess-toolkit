63b87ab8541adc33d04d85667396b5eebcda67f1
miciav <5889596+miciav@users.noreply.github.com>
2026-04-27 16:24:08 +0200

    feat: wire public API in __init__.py

--- src/shellcraft/__init__.py
+++ src/shellcraft/__init__.py
@@ -1,1 +1,34 @@
-# placeholder
+from shellcraft.backend import (
+    OutputListener,
+    RecordingShell,
+    ScriptedShell,
+    ShellBackend,
+    ShellExecutionResult,
+    SubprocessShell,
+)
+from shellcraft.fileutil import read_json_field, wrap_payload, write_json_file
+from shellcraft.net import is_port_free, pick_local_port
+from shellcraft.runners import (
+    CommandRunner,
+    ContainerRuntimeOps,
+    KubectlOps,
+    PlannedCommand,
+)
+
+__all__ = [
+    "CommandRunner",
+    "ContainerRuntimeOps",
+    "KubectlOps",
+    "OutputListener",
+    "PlannedCommand",
+    "RecordingShell",
+    "ScriptedShell",
+    "ShellBackend",
+    "ShellExecutionResult",
+    "SubprocessShell",
+    "is_port_free",
+    "pick_local_port",
+    "read_json_field",
+    "wrap_payload",
+    "write_json_file",
+]
